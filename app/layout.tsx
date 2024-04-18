c0c8f11abe85aac40d2baa11c188f42ddaa95584
rdayupay <raqueldayupay@gmail.com>
2024-04-19 02:23:03 +0800

    Initial project setup

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Esencia',
+  description:
+    'Discover timeless luxury at Esencia. Elevate your style with sophisticated designs crafted for elegance and excellence. Shop now.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
