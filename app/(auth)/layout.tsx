c4283eb5fda39d447f072496f0dc8e1aee76e828
rdayupay <raqueldayupay@gmail.com>
2024-04-23 15:28:57 +0800

    Add clerk for authentication

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
+import '../globals.css';
+
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Esencia',
+  title: 'Esencia - Admin Dashboard',
   description:
     'Discover timeless luxury at Esencia. Elevate your style with sophisticated designs crafted for elegance and excellence. Shop now.',
 };
@@ -16,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
