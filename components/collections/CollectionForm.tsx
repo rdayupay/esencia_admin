032c013fa10f3df19f96db72c1411b2a316d30ff
rdayupay <raqueldayupay@gmail.com>
2024-04-30 20:56:01 +0800

    Setup collection form route

---
+++ components/collections/CollectionForm.tsx
@@ -0,0 +1,12 @@
+import { Separator } from '../ui/separator';
+
+const CollectionForm = () => {
+  return (
+    <div className="p-10">
+      <p className="text-h2-bold">Create Collection</p>
+      <Separator className="bg-grey-1 mt-4 mb-7" />
+    </div>
+  );
+};
+
+export default CollectionForm;
