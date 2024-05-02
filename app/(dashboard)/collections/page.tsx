900385ee0fe2ce3b7fe0ae4476c9830090e540df
rdayupay <raqueldayupay@gmail.com>
2024-05-02 21:09:45 +0800

    Route button to new collection

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,5 +1,7 @@
 'use client';
+
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { Plus } from 'lucide-react';
 
 import { columns } from '@/components/collections/CollectionColumns';
@@ -8,6 +10,8 @@ import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 
 const Collections = () => {
+  const router = useRouter();
+
   const [loading, setLoading] = useState(true);
   const [collections, setCollections] = useState([]);
 
@@ -33,7 +37,10 @@ const Collections = () => {
     <div className="px-10 py-5">
       <div className="flex items-center justify-between">
         <p className="text-h2-bold">Collections</p>
-        <Button className="bg-blue-1 text-white">
+        <Button
+          className="bg-blue-1 text-white"
+          onClick={() => router.push('/collections/new')}
+        >
           <Plus className="h-4 w-4 mr-2" />
           Create Collection
         </Button>
