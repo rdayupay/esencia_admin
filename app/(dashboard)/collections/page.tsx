98893bca4043844be48f2effdbf0c60919ce991e
rdayupay <raqueldayupay@gmail.com>
2024-05-02 15:04:52 +0800

    Render the collections data in a table

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,5 +1,37 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { columns } from '@/components/collections/CollectionColumns';
+import { DataTable } from '@/components/custom-ui/DataTable';
+
 const Collections = () => {
-  return <div>Collections</div>;
+  const [loading, setLoading] = useState(true);
+  const [collections, setCollections] = useState([]);
+
+  const getCollections = async () => {
+    try {
+      const res = await fetch('/api/collections', {
+        method: 'GET',
+      });
+
+      const data = await res.json();
+      setCollections(data);
+      setLoading(false);
+    } catch (err) {
+      console.log('[collections_GET', err);
+    }
+  };
+
+  useEffect(() => {
+    getCollections();
+  }, []);
+
+  console.log(collections);
+
+  return (
+    <div>
+      <DataTable columns={columns} data={collections} />
+    </div>
+  );
 };
 
 export default Collections;
